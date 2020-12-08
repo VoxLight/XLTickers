b23246af984b151eb4f42aa2382d04b0ae09006c
VoxLight <tkkt392@gmail.com>
2020-12-08 16:15:44 -0500

    Improved effeciency and consistancy through price storage. Saving gathered prices and reusing them to save api calls and speed up the process.

--- price_updater.py
+++ price_updater.py
@@ -2,15 +2,26 @@ from toucher import touch_account_rows
 from common import globals_, get_ticker_price
 import datetime as dt
 
+price_storage = {}
+
 # 1
-def _update(ticker, price_cell, date_cell):  
+def _update(ticker, price_cell, date_cell):
     if _update_price(ticker, price_cell):
         _update_date(date_cell)
     
 # 2  
 def _update_price(ticker, cell):
     # Update the price
-    price = get_ticker_price(ticker)
+    
+    # Check if we have gotten the price before
+    if ticker in price_storage.keys():
+        # If we have, retrieve it from storage.
+        price = price_storage[ticker]
+    else:
+        # If we have not, then find the price and store it
+        price = get_ticker_price(ticker)
+        price_storage[ticker] = price
+        
     if not price:
         globals_.add_error(ticker, cell.coordinate)
         return False
