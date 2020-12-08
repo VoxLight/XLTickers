e36769b25ce0327d9393d93eea77695c53d1d567
VoxLight <tkkt392@gmail.com>
2020-12-07 20:56:46 -0500

    Module that allows the ease of opening of xl workbooks with interactive prompts.

--- opener.py
+++ opener.py
@@ -1,41 +1,42 @@
 from tkinter import filedialog
-from common import clear
+from common import clear, globals_
 from menu import menu
 import openpyxl
-import tkinter
 
 
 
 # Get the woorkbook filepath
 def _get_wb_fp():
+    print("Getting workbook...")
+    
     # Set options for retriving an excel file
     options = {
         "title": "Please select an Excel Workbook...",
         "filetypes": (("Excel Workbook", "*.xlsx"),)
     }
-    clear()
     print("Please select the workbook you would like to use.")
     return filedialog.askopenfilename(**options)
 
 
+
 # Get the sheet name inside the workbook to act on
 def _get_sheet(sheets):
-    clear()
+    print("Please select the sheet name you wish to modify.")
     return menu(sheets)
 
 
-def get_workbook():
+def get_worksheet():
     clear()
-    print("Getting workbook...")
-    
     xlfp = _get_wb_fp()
     
-    clear()
+
     print("Loading the workbook. This might take a while....")
-    wb = openpyxl.load_workbook(xlfp)
-    
+    wb = openpyxl.load_workbook(xlfp)  
+    clear()  
     sheet = _get_sheet(wb.sheetnames)
+    
     clear()
-    return wb[sheet]
+    globals_.workbook_fp = xlfp
+    return wb[sheet], wb
     
     
