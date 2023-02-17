d39a5534c7c89690494ef089132f29f895bfb47a
VoxLight <VoxLight@users.noreply.github.com>
2023-02-17 17:49:09 -0500

    Delete start.bat

--- start.bat
+++
@@ -1,9 +0,0 @@
-if [ ! -d "./venv"]; then
-    echo "No virtual environment detected, creating one."
-    py -m venv ./venv
-    echo "Installing requirements."
-    .\venv\Scripts\python.exe -m pip install -r requirements.txt
-
-echo "Starting XLTickers"
-.\venv\Scripts\python.exe main.py
-pause
