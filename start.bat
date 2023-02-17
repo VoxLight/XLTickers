e6636dd8961c8c5672c64a26d3a18b3359b7316f
VoxLight <tkkt392@gmail.com>
2023-02-17 17:32:40 -0500

    Removed venv in favor of instant venv creation

--- start.bat
+++ start.bat
@@ -1,2 +1,9 @@
+if [ ! -d "./venv"]; then
+    echo "No virtual environment detected, creating one."
+    py -m venv ./venv
+    echo "Installing requirements."
+    .\venv\Scripts\python.exe -m pip install -r requirements.txt
+
+echo "Starting XLTickers"
 .\venv\Scripts\python.exe main.py
 pause
