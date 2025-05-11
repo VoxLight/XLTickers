80f9f423e4a837dc0e696b691067e3e358b8fd10
VoxLight <tkkt392@gmail.com>
2025-05-10 20:58:30 -0400

    Change yfinance dependancy to ranaroussi cookie fix

--- start.bat
+++ start.bat
@@ -1,11 +1,12 @@
+REM Description: This batch file is used to set up and run the XLTickers application.
 @echo off
 if not exist "venv" (
-    echo No virtual environment detected, creating one.
+    echo No virtual environment detected. Creating one now.
     py -m venv venv
-    echo Installing requirements.
-    call .\venv\Scripts\activate.bat && python -m pip install -r requirements.txt
 )
+
+echo Checking dependencies
+call .\venv\Scripts\activate.bat && python -m pip install -r requirements.txt
 
 echo Starting XLTickers
 call .\venv\Scripts\activate.bat && python main.py
-pause
